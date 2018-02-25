5773998b53fdb12def319651df8243fb81a129ca
AD Slaton <slaton.ad@gmail.com>
2018-02-25 06:31:38 -0500

    adjustments for cnn-starter integration

---
+++ src/structure/server.js
@@ -0,0 +1,17 @@
+const { init } = require('./index.js'),
+    myMiddleware = (req, res, next) => {
+        res.setHeader('X-APP', 'API Starter Test');
+        return next();
+    };
+
+let config = {
+        enableGraphiql: true,
+        headers: {
+            graphiql: '"X-Good": "Yes", "X-Wow": "Indeed"'
+        },
+        middleware: [
+            myMiddleware
+        ]
+    };
+
+init(config);
